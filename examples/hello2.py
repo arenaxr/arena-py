236c867c206eaeaee9fda190371ca7ae0753adc0
Eric Riebling <er1k@oz.andrew.cmu.edu>
2020-02-21 17:38:07 -0500

    merge commit

---
+++ examples/hello2.py
@@ -0,0 +1,15 @@
+# hello2.py
+# demonstrate receiving ARENA callback (JSON) messages
+
+import arena
+
+def scene_callback(msg):
+    print("scene_callback: ", msg)
+
+arena.init("oz.andrew.cmu.edu", "realm", "hello", scene_callback)
+
+# synchronous draw commandds
+cube = arena.Object(arena.Shape.cube, clickable=True)
+
+# our main event loop
+arena.handle_events()
---
+++ examples/hello3.py
@@ -0,0 +1,18 @@
+# hello3.py
+# demonstrate setting arbitrary (animation) JSON data
+# - animates rotating duck
+import arena
+
+arena.init("oz.andrew.cmu.edu", "realm", "hello")
+arena.Object(arena.Shape.cube)
+arena.Object(arena.Shape.sphere,location=(1,1,-1),color=(255,0,0))
+arena.Object(objType=arena.Shape.gltf_model,
+             location=(-1,1,-3),
+             persist=False,
+             physics=arena.Physics.none,
+             clickable=True,
+             data='{"animation": { "property": "rotation", "to": "0 360 0", "loop": true, "dur": 10000}}',
+             url="models/Duck.glb")
+
+# our main event loop
+arena.handle_events()

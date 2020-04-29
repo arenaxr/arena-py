c2e3968330f56df3c23508d0d9d58b87432fad18
Ivan <r33t@cmu.edu>
2020-04-29 11:35:33 -0400

    Update tagsolver
    - Format w/ black
    - Don't fetch or keep clientside store of reference tags poses,
      clients will now pass reftags if available to update (otherwise,
      create with given geolocation if in builder mode)
    - Include HIGH and LOW filtering on vio motion. Builder mode requires
      more stable vio
    - Comments, debug logging
    - generate UUID off (somewhat arbitrary) hash of geo + tag id

--- demos/tagsolver.py
+++ demos/tagsolver.py
@@ -10,29 +10,31 @@ import numpy as np
 import paho.mqtt.client as mqtt
 from scipy.spatial.transform import Rotation
 
-with open('config.json') as config_file:
+with open("config.json") as config_file:
     CONFIG = json.load(config_file)
 
 CLIENT_ID = "apriltag_solver_" + str(random.randint(0, 100))
 
-HOST = CONFIG['host']
-PORT = CONFIG['port']
-TOPIC = CONFIG['default_realm'] + "/g/a/"
-TAGLOAD_URL = "https://atlas.conix.io/lookup/geo?lat=0&long=0&distance=20000&units=km&objectType=apriltag"
+HOST = CONFIG["host"]
+PORT = CONFIG["port"]
+TOPIC = CONFIG["default_realm"] + "/g/a/"
 TAG_URLBASE = "https://atlas.conix.io/record"
+DEBUG = True
+
+
+def log(*s):
+    if DEBUG:
+        print(*s)
+
 
 # fmt: off
 
-TAGS = {  # Local cache, TBD how it's invalidated or refreshed from MongoDB
-    0: {
-        "id": "Origin",
-        "pose":
-            [[1, 0, 0, 0],
-            [0, 0, 1, 0],
-            [0, -1, 0, 0],
-            [1, 0, 0, 1]],
-    }
-}
+ORIGINTAG = [
+    [1, 0, 0, 0],
+    [0, 0, 1, 0],
+    [0, -1, 0, 0],
+    [1, 0, 0, 1]
+]
 
 FLIP = [[1, 0, 0, 0],
         [0, -1, 0, 0],
@@ -44,9 +46,38 @@ FLIP = [[1, 0, 0, 0],
 RIGS = {}
 VIO_STATE = {}
 FRAME_COUNTER = {}
+ADDED_TAGS = {}
+
+VIO_MAX_DIFF_LOW = 0.05  # set diff to a more strict threshold
+VIO_MAX_DIFF_HIGH = 0.2  # set diff to a less strict threshold
+
 
 def dict_to_sns(d):
     return SimpleNamespace(**d)
+
+
+def vio_filter(vio, client_id, vio_threshold):
+    global VIO_STATE
+    # Add some extremely simple filtering based on camera motion
+    # make sure we get two readings with minimal vio movement
+    vio_pose_last = VIO_STATE.get(client_id)
+    if vio_pose_last is None:
+        log("skip, no previous camera location")
+        VIO_STATE[client_id] = vio
+        return False
+    else:
+        # Directly subtract the current and previous pose matrix
+        vio_pose_delta = np.subtract(vio, vio_pose_last)
+        # take absolute value of matrix elements and find max change value
+        vio_pose_delta = abs(vio_pose_delta)
+        vio_max_diff = vio_pose_delta.max()
+    # save state of last vio position for this camera
+    VIO_STATE[client_id] = vio
+    # Just guessed the threshold for motion
+    if abs(vio_max_diff) > vio_threshold:
+        log("Too much camera movement")
+        return False
+    return True
 
 
 def on_tag_detect(client, userdata, msg):
@@ -64,116 +95,136 @@ def on_tag_detect(client, userdata, msg):
         pos = json_msg.vio.position
         rot = json_msg.vio.rotation
 
+        # Construct pose matrix4 for camera
         vio_pose = np.identity(4)
         vio_pose[0:3, 0:3] = Rotation.from_quat(
             [rot._x, rot._y, rot._z, rot._w]
         ).as_matrix()
         vio_pose[0:3, 3] = [pos.x, pos.y, pos.z]
 
+        # Construct pose matrix for detected tag
         dtag_pose = np.identity(4)
+        # Correct for column-major format of detected tag pose
         R_correct = np.array(detected_tag.pose.R).T
         dtag_pose[0:3, 0:3] = R_correct
-        # dtag_pose[0:3, 0:3] = detected_tag.pose.R
         dtag_pose[0:3, 3] = detected_tag.pose.t
+        # Swap x/y axis of detected tag coordinate system
         dtag_pose = np.array(FLIP) @ dtag_pose @ np.array(FLIP)
 
         mqtt_response = None
 
-        # Add some extremely simple filtering based on camera motion
-        # make sure we get two readings with minimal vio movement
-        vio_max_diff=100.0;  # set diff to a large value
-        vio_pose_last=VIO_STATE.get(client_id)
-        if vio_pose_last is None: 
-             print( "skip, no previous camera location" )
-             VIO_STATE[client_id] = vio_pose
-             return
-        else:
-             # Directly subtract the current and previous pose matrix
-             vio_pose_delta=np.subtract(vio_pose,vio_pose_last)
-             # take absolute value of matrix elements and find max change value
-             vio_pose_delta=abs(vio_pose_delta)
-             vio_max_diff=vio_pose_delta.max()
-        # save state of last vio position for this camera
-        VIO_STATE[client_id] = vio_pose
-        # Just guessed the threshold for motion
-        if abs(vio_max_diff)>0.80:
-             print( "Too much camera movement" )
-             return 
-        # Just guessed the threshold for tag error 
-        if detected_tag.pose.e>5e-6:
-             print( "Too tag much error" )
-             return 
+        # Just guessed the threshold for tag error
+        if detected_tag.pose.e > 5e-6:
+            log("Too tag much error")
+            return
 
-
+        # Builder mode: solve for tag, assume client rig is previously logged
         if (
             hasattr(json_msg, "localize_tag")
             and json_msg.localize_tag
             and detected_tag.id != 0
         ):
-            print("Solve for tag", str(detected_tag.id))
-            # Solve for tag, not client
+            log("Solve for tag", str(detected_tag.id))
+
+            if not vio_filter(vio_pose, client_id, VIO_MAX_DIFF_LOW):
+                return
+
             rig_pose = RIGS.get(client_id)
             if rig_pose is None:
                 # Don't have client RIG offset, can't solve
-                print("Don't have client rig pose:", client_id)
+                log("Don't have client rig pose:", client_id)
+                return
+
+            # Calculate pose of apriltag
+            ref_tag_pose = rig_pose @ vio_pose @ dtag_pose
+            ref_tag_pos = ref_tag_pose[0:3, 3]
+            ref_tag_rotq = Rotation.from_matrix(ref_tag_pose[0:3, 0:3]).as_quat()
+
+            if not hasattr(json_msg, "geolocation"):
+                log("Builder provided no geolocation")
+                return
+            newu_uuid_name = (
+                str(detected_tag.id)
+                + "_"
+                + str(json_msg.geolocation.latitude)
+                + "_"
+                + str(json_msg.geolocation.longitude)
+            )
+            new_uuid = str(uuid.uuid5(uuid.NAMESPACE_OID, newu_uuid_name))
+
+            # either UUID is supplied from builder request, or its recently
+            # added as new
+            ref_uuid = None
+            if hasattr(json_msg, "refTag") and hasattr(json_msg.refTag, "uuid"):
+                ref_uuid = json_msg.refTag.uuid
+            elif ADDED_TAGS.get(new_uuid) is not None:
+                ref_uuid = new_uuid
+            if ref_uuid is not None:
+                log("Builder updating existing tag")
+                resp = requests.patch(
+                    TAG_URLBASE + "/" + ref_uuid,
+                    json={"pose": ref_tag_pose.tolist()},
+                    auth=HTTPBasicAuth("conix", "conix"),
+                )
             else:
-                ref_tag_pose = rig_pose @ vio_pose @ dtag_pose
-                ref_tag_pos = ref_tag_pose[0:3, 3]
-                ref_tag_rotq = Rotation.from_matrix(ref_tag_pose[0:3, 0:3]).as_quat()
-                if detected_tag.id in TAGS:
-                    print("Builder updating existing tag")
-                    # Update existing tag
-                    TAGS[detected_tag.id]["pose"] = ref_tag_pose
-                    resp = requests.patch(
-                        TAG_URLBASE + "/" + TAGS[detected_tag.id]["id"],
-                        json={"pose": ref_tag_pose.tolist()},
-                        auth=HTTPBasicAuth("conix", "conix"),
-                    )
+                log("Builder create new tag")
+                # retain tags known to be have been created as hashed by
+                # tagid, lat, long. Prevents duplicated entries (ideally)
+                if ADDED_TAGS.get(new_uuid) is not None:
+                    log("Tag already added recently")
                 else:
-                    # New tag, store it in memory and ATLAS
-                    print("Builder create new tag")
-                    TAGS[detected_tag.id] = {
-                        "id": str(uuid.uuid4()),
+                    ADDED_TAGS[new_uuid] = True
+                    new_tag = {
+                        "id": new_uuid,
                         "name": "apriltag_" + str(detected_tag.id),
                         "pose": ref_tag_pose.tolist(),
-                        "lat": 0,  # TODO: get and pass along client lat/long
-                        "long": 0,
+                        "lat": json_msg.geolocation.latitude,
+                        "long": json_msg.geolocation.longitude,
                         "ele": 0,
                         "objectType": "apriltag",
                         "url": "https://conix.io",
                     }
-                    print(TAGS[detected_tag.id])
+                    log(new_tag)
                     resp = requests.post(
-                        TAG_URLBASE,
-                        json=TAGS[detected_tag.id],
-                        auth=HTTPBasicAuth("conix", "conix"),
+                        TAG_URLBASE, json=new_tag, auth=HTTPBasicAuth("conix", "conix"),
                     )
-                mqtt_response = {
-                    "object_id": "apriltag_" + str(detected_tag.id),
-                    "action": "update",
-                    "type": "object",
-                    "data": {
-                        "position": {
-                            "x": ref_tag_pos[0],
-                            "y": ref_tag_pos[1],
-                            "z": ref_tag_pos[2],
-                        },
-                        "rotation": {
-                            "x": ref_tag_rotq[0],
-                            "y": ref_tag_rotq[1],
-                            "z": ref_tag_rotq[2],
-                            "w": ref_tag_rotq[3],
-                        },
+            mqtt_response = {
+                "object_id": "apriltag_" + str(detected_tag.id),
+                "action": "create",
+                "type": "object",
+                "data": {
+                    "object_type": "cube",
+                    "position": {
+                        "x": ref_tag_pos[0],
+                        "y": ref_tag_pos[1],
+                        "z": ref_tag_pos[2],
                     },
-                }
+                    "rotation": {
+                        "x": ref_tag_rotq[0],
+                        "y": ref_tag_rotq[1],
+                        "z": ref_tag_rotq[2],
+                        "w": ref_tag_rotq[3],
+                    },
+                    "scale": {"x": 0.15, "y": 0.15, "z": 0.02},
+                    "color": "#ff0000",
+                },
+            }
         else:  # Solving for client rig, default localization operation
-            print("Localizing", client_id, "on", str(detected_tag.id))
-            ref_tag = TAGS.get(detected_tag.id)
-            if ref_tag is None:
-                # Tag not found. TODO: query ATLAS for it
-                print("Tag not found, not in build mode")
+            log("Localizing", client_id, "on", str(detected_tag.id))
+            if not vio_filter(vio_pose, client_id, VIO_MAX_DIFF_HIGH):
                 return
-            ref_tag_pose = ref_tag["pose"]
+            if detected_tag.id == 0:
+                ref_tag_pose = ORIGINTAG
+            elif hasattr(json_msg, "refTag"):
+                # comes in as col-major
+                arr = json_msg.refTag.pose.elements
+                ref_tag_pose = np.array(
+                    [arr[0:4], arr[4:8], arr[8:12], arr[12:16]]
+                ).T
+            else:
+                # Tag not found. TODO: query ATLAS for it
+                log("Tag not found, not in build mode")
+                return
             rig_pose = ref_tag_pose @ np.linalg.inv(dtag_pose) @ np.linalg.inv(vio_pose)
             rig_pos = rig_pose[0:3, 3]
             rig_rotq = Rotation.from_matrix(rig_pose[0:3, 0:3]).as_quat()
@@ -219,20 +270,4 @@ def start_mqtt():
     mttqc.loop_forever()
 
 
-r = requests.get(TAGLOAD_URL)
-loaded_tags = r.json()
-for tag in loaded_tags:
-    if tag["name"][0:9] == "apriltag_":
-        tagid = int(tag["name"][9:])
-        TAGS[tagid] = {
-            "id": tag["id"],
-            "name": tag["name"],
-            "pose": tag["pose"],
-            "lat": tag["lat"],
-            "long": tag["long"],
-            "ele:": tag["ele"],
-            "objectType": "apriltag",
-            "url": tag["url"],
-        }
-        print("Loaded tag", str(tagid))
 start_mqtt()

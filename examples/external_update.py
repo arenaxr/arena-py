425fd15224bbf4c80164a08f184bbb54f09ad417
Michael Farb <mwfarb@andrew.cmu.edu>
2020-07-09 22:21:23 -0400

    adding child to obey rotation

--- examples/external_update.py
+++ examples/external_update.py
@@ -6,10 +6,15 @@
 import arena
 
 arena.init("oz.andrew.cmu.edu", "realm", "external")
-arena.Object(objName="apriltag_450",
+TAG = arena.Object(objName="apriltag_450",
+                   objType=arena.Shape.cube,
+                   data='{"material": {"transparent": true, "opacity": 0}}',
+                   persist=True)
+arena.Object(objName="duck",
              objType=arena.Shape.gltf_model,
              scale=(0.1, 0.1, 0.1),
-             rotation=(0, 0, 0, 1),
+             rotation=(0.7, 0, 0, 0.7),
+             parent=TAG.objName,
              persist=True,
              url="models/Duck.glb")
 

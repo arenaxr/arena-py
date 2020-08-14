a64998915e7142f454448846861eee91a88dacde
CalculatorSP <mike.handidate@gmail.com>
2020-08-14 13:14:01 -0400

    [update] parse args

--- demos/gt-sync.py
+++ demos/gt-sync.py
@@ -1,54 +1,53 @@
-# volatile.py
-''' Demonstrate setting an object to be a child of a camera 
+''' Synchronize users for ground truth collection with apriltags
 '''
 import arena
 import sys
 import getopt
 
-scene = "cic-tags"
-user = "test"
-color = (0, 255, 0)
+broker = 'oz.andrew.cmu.edu'
+realm = 'realm'
+scene = 'cic-tags'
+COLOR_WALK = (0, 255, 0)
+COLOR_FINDTAG = (255, 0, 0)
+COLOR_WAIT = (255, 255, 0)
 
+def printhelp():
+    print('gt-sync.py [-s <scene>] <user1> [user2 ...]')
+    print('   ex: python3 gt-sync.py -s myScene name1 name2 name3')
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hs:u:c:", [
-                               "scene=", "user=", "color="])
+    opts, args = getopt.getopt(sys.argv[1:], 'hs:', ['scene='])
 except getopt.GetoptError:
-    print("camera-child.py -s <scene> -u <user> -c <r,g,b> ")
-    print("   ex: python3 camera-child.py -s myScene -u myName -c 255,0,0")
+    printhelp()
     sys.exit(2)
 
 for opt, arg in opts:
     if opt == '-h':
-        print("camera-child.py -s <scene> -u <user> -c <r,g,b> ")
-        print("   ex: python3 camera-child.py -s myScene -u myCamera -c 255,0,0")
+        printhelp()
         sys.exit()
-    elif opt in ("-s", "--scene"):
+    elif opt in ('-s', '--scene'):
         scene = arg
-    elif opt in ("-u", "--user"):
-        user = arg
-    elif opt in ("-c", "--color"):
-        color = arg.split(',')
 
+if len(args) < 1:
+    printhelp()
+    sys.exit(2)
 
 print("Scene: " + scene)
-print("User: " + user)
-print("Color: " + str(color))
+print("Users: " + args)
 
-arena.init("oz.andrew.cmu.edu", "realm", scene)
-obj_str = "circle_" + user
-camera_str = "camera_" + user + "_" + user
-arena.Object(objName=obj_str,
-             objType=arena.Shape.circle,
-             parent=camera_str,
-             location=(-.5, 0, -.5),
-             rotation=(0, 0, 0, 1),
-             scale=(0.05, 0.05, 0.05),
-             color=color,
-             persist=True)
+# arena.init(broker, realm, scene)
+# obj_str = "circle_" + user
+# camera_str = "camera_" + user + "_" + user
+# arena.Object(objName=obj_str,
+#              objType=arena.Shape.circle,
+#              parent=camera_str,
+#              location=(-.5, 0, -.5),
+#              rotation=(0, 0, 0, 1),
+#              scale=(0.05, 0.05, 0.05),
+#              color=color,
+#              persist=True)
 
-print("Go to URL: https://xr.andrew.cmu.edu/?scene=" +
-      scene + "&fixedCamera=" + user)
+# print("Go to URL: https://xr.andrew.cmu.edu/?scene=" + scene + "&fixedCamera=" + user)
 
-# our main event loop
-arena.handle_events()
+# # our main event loop
+# arena.handle_events()

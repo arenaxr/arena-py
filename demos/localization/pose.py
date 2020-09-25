10980f8d59805980398c033f3a26a2d781daaaba
CalculatorSP <mike.handidate@gmail.com>
2020-09-25 14:32:12 -0400

    [bugfix] coordinate error in resolving pose ambiguity

--- demos/localization/pose.py
+++ demos/localization/pose.py
@@ -50,7 +50,7 @@ def resolve_pose_ambiguity(pose1, err1, pose2, err2, vio, tagpose):
     vertical_vector = tagpose[0:3, 0:3].T @ np.array([[0, 1, 0]]).T
     pose1_vertical = pose1[0:3, 0:3] @ vertical_vector
     pose2_vertical = pose2[0:3, 0:3] @ vertical_vector
-    vio_vertical = vio[0:3, 0:3].T @ vertical_vector
+    vio_vertical = vio[0:3, 0:3].T @ np.array([[0, 1, 0]]).T
     pose1_vertical = pose1_vertical / np.linalg.norm(pose1_vertical)
     pose2_vertical = pose2_vertical / np.linalg.norm(pose2_vertical)
     vio_vertical = vio_vertical / np.linalg.norm(vio_vertical)

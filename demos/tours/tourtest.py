ef6d72d796399842f777938dd895583b133cbcfb
court <cjk74@pitt.edu>
2020-10-23 13:32:11 -0400

    added sign

--- demos/tours/tourtest.py
+++ demos/tours/tourtest.py
@@ -9,12 +9,16 @@ import math
 HOST = "arena.andrew.cmu.edu"
 SCENE = "roomtest5"
 
+sign_links =  ['Link 1','https://3dwarehouse.sketchup.com/model/f2991e0c12644c9ff87d99a411d2b1c5/University-of-Southern-California-USC-Tower?hl=en','Link 2','https://www.f1.com','Link 3','https://www.eet.com']
+
 waypoints = []
-waypoints.append((0.0,0.1,0.0))
-waypoints.append((-14.8,0.1,-1.4))
-waypoints.append((-10.8,0.1,5.3))
-waypoints.append((-9.8,0.1,37.1))
-waypoints.append((-16.3,0.1,18.8))
+waypoints.append((0.0, 0.2, 0.0))
+waypoints.append((-15.8, 0.2, -3.0))
+waypoints.append((-1.3, 0.2, 6.4))
+waypoints.append((-0.5, 0.4, 33.9))
+waypoints.append((-11.5, 0.2, 38.7))
+waypoints.append((-18.1, 0.2, 48.7))
+waypoints.append((-46.4, 0.3, 44.1))
 
 def drawpath(waypoints, name_seed, persist ):
     print( waypoints)
@@ -42,11 +46,24 @@ def drawpath(waypoints, name_seed, persist ):
             arena.Object(objType=arena.Shape.circle, 
             objName=pathobjstr, 
             location=(x,y,z), 
-            color=(0,255,255),scale=(0.1,0.1,0.1),
+            color=(0,255,255),scale=(0.1,0.1,1),
             rotation=(-0.7,0.0,0.0,0.7), 
             data='{"material": {"transparent":true,"opacity": 0.3}}', 
             persist=persist);
         lastPt = pt 
+
+
+def signs():
+    arena.Object(
+            objName="signpost1",
+            url="store/users/wiselab/models/signpost/scene.gltf",
+            objType=arena.Shape.gltf_model,
+            scale=(0.1, 0.1, 0.1),
+            rotation=(0, 0.3, 0, 1),
+            location=(-18.2, 0.2, -2.3),
+            clickable=True,
+            persist=True
+    )
 
 
 arena.init(HOST, "realm", SCENE)
@@ -54,4 +71,5 @@ arena.init(HOST, "realm", SCENE)
 
 print("starting main loop")
 drawpath(waypoints, 0, True)
+signs()
 arena.handle_events()

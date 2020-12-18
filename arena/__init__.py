19a810a51386f1912140774d563296a67afdf747
Edward <lued2018@gmail.com>
2020-12-18 01:55:56 -0500

    add __init__.py

---
+++ arena/__init__.py
@@ -0,0 +1,12 @@
+"""
+ARENA-py
+
+Draw objects in the ARENA using Python!
+"""
+
+__version__ = "0.1.0"
+__author__ = "Conix Research Center"
+__credits__ = "WiSE Lab, Carnegie Mellon University"
+
+from .arena import *
+from .auth import *

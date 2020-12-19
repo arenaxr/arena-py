3f496cb78e966792ccb60f8770d5b5f16ac2b92a
Edward <lued2018@gmail.com>
2020-12-19 16:47:41 -0500

    add documentation

---
+++ examples-0.1.0/basic.py
@@ -0,0 +1,14 @@
+from arena import *
+
+# setup library
+arena = Arena("arena.andrew.cmu.edu", "example", "realm")
+
+def main():
+    # make a cube
+    cube = Cube(object_id="my_cube", position=Position(0,4,-2), scale=Scale(2,2,2))
+    # add the cube
+    arena.add_object(cube)
+
+# add and start tasks
+arena.run_once(main)
+arena.start_tasks()

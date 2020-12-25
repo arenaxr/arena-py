ab586a095dfed41949d29ef23b9e688176e17a21
Edward <lued2018@gmail.com>
2020-12-25 14:41:04 -0600

    no errors when terminating event loop; all tasks wait for mqtt to connect

--- examples-0.1.0/beginner.py
+++ examples-0.1.0/beginner.py
@@ -6,6 +6,7 @@ arena = Arena("arena.andrew.cmu.edu", "example", "realm")
 def main():
     # make a cube
     cube = Cube(object_id="my_cube", position=Position(0,4,-2), scale=Scale(2,2,2))
+    print(cube.json())
     # add the cube
     arena.add_object(cube)
 

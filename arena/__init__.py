74f05a14b69d0f8fae0de1df7a77be3b40aab99d
Edward <lued2018@gmail.com>
2021-01-22 16:31:22 -0600

    Update __init__.py

--- arena/__init__.py
+++ arena/__init__.py
@@ -6,7 +6,6 @@ Draw objects in the ARENA using Python!
 
 __version__     = "0.1.0"
 __author__      = "Conix Research Center"
-__credits__     = ["Edward Lu", "Eric Riebling", "Michael Farb", "Nuno Pereira"]
 
 __license__     = "BSD 3-Clause"
 __maintainer__  = "WiSE Lab, Carnegie Mellon University"

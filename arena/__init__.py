f697129543c7e9b9cb61697f05af87b84973d928
Edward <lued2018@gmail.com>
2021-01-27 12:37:39 -0600

    update version

--- arena/__init__.py
+++ arena/__init__.py
@@ -4,7 +4,7 @@ ARENA-py
 Draw objects in the ARENA using Python!
 """
 
-__version__     = "0.1.2"
+__version__     = "0.1.3"
 __author__      = "CONIX Research Center"
 
 __license__     = "BSD 3-Clause"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="arena-py",
-    version="0.1.2",
+    version="0.1.3",
     author="Conix Research Center",
     author_email="info@conix.io",
     license="BSD 3-clause \"New\" or \"Revised License\"",

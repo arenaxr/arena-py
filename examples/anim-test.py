7cadd3d6afcdd3cd6cae2e02b6ee6407c66e7eed
Anthony Rowe <agr@ece.cmu.edu>
2021-01-31 23:36:12 -0500

    added morph targets

--- examples/anim-test.py
+++ examples/anim-test.py
@@ -16,30 +16,66 @@ def make_xr_logo():
     )
     arena.add_object(xr_logo)
 
+def close_eye():
+    morph = {
+        "gltf-morph__0": {
+            "morphtarget": "eyeTop",
+            "value": "0.8" 
+        },
+        "gltf-morph__1": {
+            "morphtarget": "eyeBottom",
+            "value": "0.8" 
+        }
+    }
+    return morph
+
+def open_eye():
+    morph = {
+        "gltf-morph__0": {
+            "morphtarget": "eyeTop",
+            "value": "0.0" 
+        },
+        "gltf-morph__1": {
+            "morphtarget": "eyeBottom",
+            "value": "0.0" 
+        }
+    }
+    return morph
+
+
+
 @arena.run_forever(interval_ms=2500)
 def periodic():
     global x
     global xr_logo    # non allocated variables need to be global
 
-    if x%4==0:
-        # Move object back to start (capturing previous location from animation)
-        # When using animated positions, don't mix with standard Position( ) object
-        arena.update_object(xr_logo, animation=Animation(property="position", start=(0,0,-10), end=(0,0,-5),easing="linear",dur=0 ) )
+    if x%7==0:
         # Trigger a single "wave" animation
         arena.update_object(xr_logo, animation_mixer=Animation(clip="wave", loop="once" ) )
         print( "Wave Once")
-    if x%4==1:
+    if x%7==1:
         arena.update_object(xr_logo, animation_mixer=Animation(clip="rotate",loop="once" ) )
         print( "Rotate Once")
-    if x%4==2:
+    if x%7==2:
         # Test wildcard for multiple clips
         arena.update_object(xr_logo, animation_mixer=Animation(clip="*", loop="repeat" ) )
         print( "Wave and Rotate Repeat")
-    if x%4==3:
+    if x%7==3:
         arena.update_object(xr_logo, animation_mixer=Animation(clip="*",loop="repeat" ) )
         arena.update_object(xr_logo, animation=Animation(property="position", start=(0,0,-5), end=(0,0,-10),easing="linear",dur=1000 ) )
         arena.update_object(xr_logo, animation__2=Animation(property="rotation", start=(0,0,0), end=(0,180,0),easing="linear",dur=1000 ) )
         print( "Wave and Rotate Repeat and move with tweening")
+    if x%7==4:
+        # Move object back to start (capturing previous location from animation)
+        # When using animated positions, don't mix with standard Position( ) object
+        arena.update_object(xr_logo, animation_mixer=Animation(clip="*",loop="once" ) )
+        arena.update_object(xr_logo, animation=Animation(property="position", start=(0,0,-10), end=(0,0,-5),easing="linear",dur=0 ) )
+    if x%7==5:
+        arena.update_object(xr_logo, **close_eye()  )
+        print( "Morph Target Close Eye")
+    if x%7==6:
+        arena.update_object(xr_logo, **open_eye()  )
+        print( "Morph Target Open Eye")
 
     x=x+1
 

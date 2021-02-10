bc5c863f3d23126ca437f24eeec67ce7bcf61575
Edward <lued2018@gmail.com>
2021-02-10 13:29:46 -0500

    update version

--- arena/__init__.py
+++ arena/__init__.py
@@ -4,7 +4,7 @@ ARENA-py
 Draw objects in the ARENA using Python!
 """
 
-__version__     = "0.1.6"
+__version__     = "0.1.7"
 __author__      = "CONIX Research Center"
 
 __license__     = "BSD 3-Clause"

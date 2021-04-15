472169c0b85bcd7463aca8bb989c7040d5e20ef2
Anthony Rowe <agr@ece.cmu.edu>
2021-04-14 20:14:18 -0400

    updated headbanger

--- tools/tests/headbanger.py
+++ tools/tests/headbanger.py
@@ -1,6 +1,6 @@
 from arena import *
 import random
-
+import datetime 
 
 scene = Scene(host="arenaxr.org", realm="realm", scene="headbanger", )
 # Create models
@@ -19,18 +19,22 @@ for x in range(100):
 
 i=0
 cnt=0
+last = datetime.datetime.now()
 
 @scene.run_once
 def main():
    scene.add_object(head)
 
-@scene.run_forever(interval_ms=10)
+@scene.run_forever(interval_ms=75)
 def update():
-    global i, headlist, cnt
+    global i, headlist, cnt, last
     for head in headlist:
         scene.update_object( head, rotation=(0, i, 0),)
-    i=i+10
+    i=i+15
     cnt=cnt+1
-    print("Tick: " + str(cnt))
+    now = datetime.datetime.now()
+    c = now-last
+    last=now
+    print("Tick: " + str(cnt) + " Time: " + str(c.microseconds/1000))
 
 scene.run_tasks()

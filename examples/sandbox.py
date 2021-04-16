8bb72632b531e91253e242338bd6d4edff0fea56
Denise-Yang <47067329+Denise-Yang@users.noreply.github.com>
2021-04-16 01:19:09 -0400

    boards communicate w each other

--- examples/sandbox.py
+++ examples/sandbox.py
@@ -5,24 +5,29 @@ scene = Scene(host="arenaxr.org", realm="realm", scene="example")
 taPos = Position(-10,0,-10)
 text = Text(object_id ="label", text="TA BOARD", position=Position(.25,6,4), rotation=Rotation(0,270,0))
 taB = Box(object_id="my_shape1", position=Position(.25,4, 4),scale=Scale(.25,2,4), persist=True)
-button2 = Cylinder(object_id="my_button1", position=Position(.1,3.3,5.5),scale=Scale(.1,.05,0.1),rotation=Rotation(0,0,90), persist=True)
+button2 = Cylinder(object_id="m_button1", position=Position(.1,3.3,5.5),scale=Scale(.1,.05,0.1),rotation=Rotation(0,0,90), persist=True)
 
 
-class QuestionBoard(Object):
-    def __init__(self,scene, max_display, pos):
+class DisplayBoard(Object):
+    def __init__(self,scene, board, max_display):
         self.start_display = 0
         self.end_display = 0
         self.display_amt = max_display
         self.q = []
         self.size = 0
-        self.board = Box(object_id="my_shape", position=pos,scale=Scale(.25,2,4), persist=True)
-        self.button = Cylinder(object_id="my_button", position=Position(.1,3.3,-1.5),scale=Scale(.1,.05,0.1),rotation=Rotation(0,0,90), persist=True)
+        self.board= board
+        #self.board = Box(object_id="my_shape", position=pos,scale=Scale(.25,2,4), persist=True)
+        pass
 
     def insert(self, student, question):
-        self.q.append((student,Text(object_id ="my_text"+str(self.size), text=student, rotation=Rotation(0,270,0), scale=Scale(.25,.25,0.25), parent=queue.board)))
+        self.q.append((student,Text(object_id ="my_text"+str(self.size), text=student, rotation=Rotation(0,270,0), scale=Scale(.25,.25,0.25), parent=self.board)))
         self.size += 1 
         if self.size < self.display_amt:
             self.end_display += 1
+        if self.size >= self.display_amt:
+            self.start_display += 1
+
+        
     def dequeue(self, student):
         if self.size <= 0:
             print("queue empty!")
@@ -50,38 +55,69 @@ class QuestionBoard(Object):
     def get_display_list(self):
         return self.q[self.start_display:self.start_display + self.end_display]
 
-class TABoard(Object):
-    def __init__(self,scene,QBoard, position, max_display):
-        self.start_display = 0
-        self.end_display = 0
-        self.display_amt = max_display
-        self.Q = []
-        self.size = 0
-        self.scene = scene
-        for i in range(len(QBoard)):
-            self.Q.append((Qboard[i][0], QBoard[i][1], Cylinder(object_id="my_button",scale=Scale(.1,.05,0.1),rotation=Rotation(0,0,90), persist=True)))
-    def display(self):
-        #get elements to display and display that text w a button next to it
-        #button should teleport user id associated with question to the TA
-        #after clicking button the question should be removed from the queue
-       
-        display_list = self.Q.get_display_list()
+class TABoard(DisplayBoard):
+    def __init__(self,scene, board, max_display):
+        super().__init__(scene,board,max_display)
+        self.id_ct = 0
+        self.q = []
+        self.board = board
+        #inherit queue from parent QB
+        #id_ct
+        pass
+    def display(self):   
+        display_list = self.get_display_list()
         for i in range(0,len(display_list)): 
-            self.scene.update_object(display_list[i][1],position=Position(-1.5, .25 -0.1*i, 0))
-            self.scene.update_object(display_list[i][2],position=Position(-1.5, .25 -0.1*i, -1.5))
-    def dequeue(self, student):
-        if self.size <= 0:
-            print("queue empty!")
-            return
-        (student,question) = self.q.pop()
-        self.size -= 1
+            scene.update_object(display_list[i][1],position=Position(-1.5, .25 -0.1*i, 0))
+            scene.update_object(display_list[i][2],position=Position(-1.2, .25 -0.1*i, .45),scale=Scale(.05,.05,.1*.25))
+    def insert(self,student,question):
+        #add button to associate with q
+        #inc id_ct
+
+        self.q.append((student,Text(object_id ="my_text_t"+str(self.size), text=student, rotation=Rotation(0,270,0), scale=Scale(.25,.25,0.25), parent=self.board),Cylinder(object_id="my_button"+str(self.size), rotation=Rotation(0,0,90), parent= self.board)))
+        self.size += 1 
         if self.size < self.display_amt:
-            self.end_display -= 1
-        return (student,question)
+            self.end_display += 1
 
+    def dequeue(self, student):
+        #pop entry from q
+        pass
 
-queue = QuestionBoard(scene,5,Position(.25,4,-3))
+class QBoard(DisplayBoard):
+    def __init__(self,scene, board, max_display):
+        super().__init__(scene,board, max_display)
+        self.TABoards = []
+        self.board = board
+        self.button = Cylinder(object_id="my_button", position=Position(.1,3.3,-1.5),scale=Scale(.1,.05,0.1),rotation=Rotation(0,0,90), persist=True)
+        #id_ct
+    def insert(self, student, question):
+        self.q.append((student,Text(object_id ="my_text"+str(self.size), text=student, rotation=Rotation(0,270,0), scale=Scale(.25,.25,0.25), parent=self.board)))
+        self.size += 1 
+        if self.size < self.display_amt:
+            self.end_display += 1
+        for i in range(len(self.TABoards)):
+            self.TABoards[i].insert(student,question)
 
+        #loop through TA boards and call updateinsrt
+        #inc id_ct
+        pass
+    def addTABoard(self, ta_board):
+        self.TABoards.append(ta_board)
+     
+    def delTABoard(self):
+        #delete ta board
+        pass
+    def dequeue(self, student):
+        #pop entry from q, update all TA lists
+        pass
+    def display(self):
+       
+        display_list = self.get_display_list()
+        for i in range(0,len(display_list)): 
+            scene.update_object(display_list[i][1],position=Position(-1.5, .25 -0.1*i, 0))
+
+queue = QBoard(scene,Box(object_id="my_shape", position=Position(.25,4,-3),scale=Scale(.25,2,4), persist=True),5);
+taBoard = TABoard(scene, taB,5)
+queue.addTABoard(taBoard)
 """
 create Qboard object
    saves object_ids of students when they put their questions on the queue
@@ -115,30 +151,33 @@ def setup():
     scene.add_object(queue.button)
     scene.add_object(queue.board)
     scene.add_object(button2)
-    scene.add_object(taB)
+    scene.add_object(taBoard.board)
     scene.add_object(text)
 
 @scene.run_forever(interval_ms=100)
 def main():
     global queue
     global scene
-    global taPos
+    global taBoard
     def mouse_handler(scene, evt,msg):
         #
         if evt.type == "mousedown" and clickB(evt.data.clickPos, queue.button.data.position,1):
             print("adding ", msg["data"]["source"])
             queue.insert(msg["data"]["source"],"a")
-            #teleport(taPos, msg["data"]["source"])
-
-            display_list = queue.get_display_list()
-            for i in range(0,len(display_list)): 
-                scene.update_object(display_list[i][1],position=Position(-1.5, .25 -0.1*i, 0))
+            queue.display()
+            taBoard.display()
     def ta_mouse_handler(scene, evt, msg):
-        if evt.type == "mousedown" and clickB(evt.data.clickPos, button2.data.position,1):
-            teleport(taPos, msg["data"]["source"])
+        if evt.type == "mousedown":
+            for obj in(taBoard.get_display_list()):
+                #print(obj[2]['data']['position'])
+                #print(evt['data']['clickPos'])
+                #print(obj[2]['data']['position'].distance_to(evt['data']['clickPos']))
+                if obj[2]['data']['position'].distance_to(evt['data']['clickPos']) <20:
+                    teleport(taPos, msg["data"]["source"])
 
     scene.update_object(queue.button, click_listener=True, evt_handler=mouse_handler)
-    scene.update_object(taB, click_listener=True, evt_handler=ta_mouse_handler)
+    for obj in (taBoard.get_display_list()):
+        scene.update_object(obj[2], click_listener=True, evt_handler=ta_mouse_handler)
 
 
 scene.run_tasks()

9f7798aa76ced28292464c30212fc57c461b810b
mwfarb <mwfarb@andrew.cmu.edu>
2021-04-22 13:49:55 -0400

    Load static users into dict

--- tools/badges/badges.py
+++ tools/badges/badges.py
@@ -3,6 +3,8 @@
 # Application responds to users in the scene and updates the user avatar with name and badge updates.
 import argparse
 import csv
+import io
+import json
 
 import requests
 from arena import *
@@ -10,36 +12,42 @@ from arena import *
 BROKER = "arenaxr.org"
 REALM = "realm"
 SCENE = "badges-example"
+USERS = {}
 
 
 def init_args():
-    global BROKER, REALM, SCENE
-    parser = argparse.ArgumentParser(description="ARENA example badges manager.")
+    global BROKER, REALM, SCENE, USERS
+    parser = argparse.ArgumentParser(
+        description="ARENA badges manager example.")
     parser.add_argument(
         "-u", "--userfile", type=str, help="CSV delimited user list")
     args = parser.parse_args()
     print(args)
 
-    with requests.Session() as s:
-        download = s.get(args.userfile)
-        decoded_content = download.content.decode('utf-8')
-        cr = csv.reader(decoded_content.splitlines(), delimiter=',')
-        my_list = list(cr)
-        for row in my_list:
-            print(row)
+    r = requests.get(args.userfile)
+    buff = io.StringIO(r.text)
+    dr = csv.DictReader(buff)
+    for row in dr:
+        key = row["Username"]
+        USERS[key] = row
+    print(USERS)
 
 
 def scene_callback(scene, obj, msg):
     print("on_msg_callback "+str(obj))
 
+
 def user_join_callback(scene, obj, msg):
     print("user_join_callback "+str(obj))
+
 
 def user_left_callback(scene, obj, msg):
     print("user_left_callback "+str(obj))
 
+
 def end_program_callback(scene, obj, msg):
     print("end_program_callback "+str(obj))
+
 
 # parse args and wait for events
 init_args()
@@ -49,8 +57,8 @@ scene = Scene(
     realm=REALM,
     scene=SCENE,
     on_msg_callback=scene_callback,
-    user_join_callback = user_join_callback,
-    user_left_callback = user_left_callback,
+    user_join_callback=user_join_callback,
+    user_left_callback=user_left_callback,
     end_program_callback=end_program_callback,
     **kwargs)
 scene.run_tasks()

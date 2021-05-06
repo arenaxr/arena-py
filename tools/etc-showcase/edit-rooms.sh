d0c3fbf3f16d0a9a22ff44eabc0136800d9e0a3f
Nuno Pereira <nampereira@gmail.com>
2021-05-05 22:16:54 -0400

    Room list

--- tools/etc-showcase/edit-rooms.sh
+++ tools/etc-showcase/edit-rooms.sh
@@ -3,7 +3,7 @@
 # NOTE: iterations of a while ** in a pipeline ** are executed inside a subshell
 # see: http://mywiki.wooledge.org/BashFAQ/024
 cat project-rooms.txt | while read room; do
-  export NAMESPACE=npereira
+  export NAMESPACE=etc
   export SCENE=$room
   python3 edit-room.py
 done

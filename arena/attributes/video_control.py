4fc30e447ebef154f5add9de18aba7dade0f7f91
John Choi <johnchoi313@gmail.com>
2023-04-11 11:52:46 -0400

    removed args
    apparently args not needed

--- arena/attributes/video_control.py
+++ arena/attributes/video_control.py
@@ -5,6 +5,3 @@ class VideoControl(Attribute):
     VideoControl Attribute.
     Usage: video_control=VideoControl(...)
     """
-    def __init__(self, video_object, video_path, anyone_clicks=True, video_loop=True, autoplay=True, volume=1.0):
-        super().__init__(video_object=video_object, video_path=video_path, 
-                         anyone_clicks=anyone_clicks, video_loop=video_loop, autoplay=autoplay, volume=volume)

0a273479407b8641a02d4b135240670dd7efe0e9
Ivan Liang <r33t@cmu.edu>
2023-06-23 14:12:25 -0400

    Add to Animation example, multiple objs

--- examples/attributes/animation.py
+++ examples/attributes/animation.py
@@ -1,18 +1,39 @@
 from arena import *
 
-scene = Scene(host="arenaxr.org", scene="example")
+scene = Scene(host="arenaxr.org", scene="example", debug=True)
 
-my_torus = Torus(
-    object_id="my_torus",
-    position=(0,2,-5),
-    scale=(1.0,1.0,1.0),
+my_torus1 = Torus(
+    object_id="my_torus1",
+    position=(0, 2, 0),
+    radius=1,
+    persist=True,
 )
 
-@scene.run_once
-def rotate_torus():
-    my_torus.dispatch_animation(
-            Animation(property="rotation",start=(0,0,0),end=(0,360,0),easing="linear",dur=1000)
-        )
-    scene.update_object(my_torus)
+my_torus2 = Torus(
+    object_id="my_torus2",
+    position=(0, 4, 0),
+    radius=0.5,
+    persist=True,
+)
 
+
+@scene.run_once
+def move_torus1():
+    my_torus1.dispatch_animation(
+        Animation(property="Position", start=(0, 2, 0), end=(-5, 2, -5),
+                  easing="linear", dur=8000)
+    )
+    scene.update_object(my_torus1)
+
+@scene.run_once
+def move_torus2():
+    my_torus2.dispatch_animation(
+        Animation(property="Position", start=(0, 4, 0), end=(-5, 4, -5),
+                  easing="easeInQuad", dur=4000)
+    )
+    scene.update_object(my_torus2)
+
+
+scene.add_object(my_torus1)
+scene.add_object(my_torus2)
 scene.run_tasks()

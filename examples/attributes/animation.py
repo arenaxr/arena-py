a459210e92ba96166b6d47035e7bef8775f832eb
Ivan Liang <r33t@cmu.edu>
2023-06-26 15:39:32 -0400

    Update Animation example
    - Use Position/Rotation

--- examples/attributes/animation.py
+++ examples/attributes/animation.py
@@ -5,7 +5,7 @@ scene = Scene(host="arenaxr.org", scene="example", debug=True)
 my_torus1 = Torus(
     object_id="my_torus1",
     position=(0, 2, 0),
-    radius=1,
+    radius=0.75,
     persist=True,
 )
 
@@ -20,11 +20,11 @@ my_torus2 = Torus(
 @scene.run_once
 def move_torus1():
     my_torus1.dispatch_animation(
-        Animation(property="Position", start=(0, 2, 0), end=(-5, 2, -5),
+        Animation(property="position", start=Position(0, 2, 0), end=Position(-5, 2, -5),
                   easing="linear", dur=8000)
     )
     my_torus1.dispatch_animation(
-        Animation(property="Rotation", start=(0, 0, 0), end=(0, 360, 0),
+        Animation(property="rotation", start=Rotation(0, 0, 0), end=Rotation(0, 360, 0),
                   easing="linear", dur=2000, loop=True)
     )
     scene.update_object(my_torus1)
@@ -33,17 +33,23 @@ def move_torus1():
 @scene.run_once
 def move_torus2():
     my_torus2.dispatch_animation(
-        Animation(property="Position", start=(0, 4, 0), end=(-5, 4, -5),
+        Animation(property="position", start=Position(0, 4, 0), end=Position(-5, 4, -5),
                   easing="easeInQuad", dur=4000)
     )
     scene.update_object(my_torus2)
 
 
 def cancel_torus1_move():
-    scene.update_object(my_torus1, Position=(-6, 2, -6))  # Move to new spot
+    scene.update_object(my_torus1, position=Position(-8, 2, -5))  # Move to new spot
+
+
+def change_torus2_color():
+    scene.update_object(my_torus2, color=Color(255, 0, 0))  # Change color to red
 
 
 scene.add_object(my_torus1)
 scene.add_object(my_torus2)
+print("Moving both shapes in same direction")
 scene.run_after_interval(cancel_torus1_move, 5000)
+scene.run_after_interval(change_torus2_color, 2000)
 scene.run_tasks()

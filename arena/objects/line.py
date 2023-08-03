5b24a40a8342b110421998781caa975b0c983c7e
Edward Lu <lued2018@gmail.com>
2023-08-03 15:11:15 -0400

    fix: put back default start and end in Line

--- arena/objects/line.py
+++ arena/objects/line.py
@@ -7,6 +7,5 @@ class Line(Object):
     """
     object_type = "line"
 
-    def __init__(self, start, end, **kwargs):
+    def __init__(self, start=(0,0,0), end=(10,10,10), **kwargs):
         super().__init__(object_type=Line.object_type, start=start, end=end, **kwargs)
-

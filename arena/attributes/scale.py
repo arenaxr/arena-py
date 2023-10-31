e6c705b94ca0417e488c9af8c79594737038daf2
Ivan Liang <r33t@cmu.edu>
2023-10-31 12:52:41 -0400

    Validate pose classes args
    - Check args length is at least 3
    - Ensure first arg is not an iterable (input as tuple, list, nparr)

--- arena/attributes/scale.py
+++ arena/attributes/scale.py
@@ -1,12 +1,20 @@
 from ..utils import Utils
 from .attribute import Attribute
+from collections.abc import Iterable
+
 
 class Scale(Attribute):
     """
     Scale Attribute.
     Usage: scale=Scale(x,y,z)
     """
-    def __init__(self, x=1, y=1, z=1):
+
+    def __init__(self, x=None, y=None, z=None):
+        if x is not None and (y is None or z is None or isinstance(x, Iterable)):
+            raise ValueError("Scale takes x,y,z")
+        x = x or 1
+        y = y or 1
+        z = z or 1
         super().__init__(x=Utils.agran(x), y=Utils.agran(y), z=Utils.agran(z))
 
     def to_str(self):

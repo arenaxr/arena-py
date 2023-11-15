d7a907a141f99349ef3f313c246c2f5367fe8d43
Ivan Liang <r33t@cmu.edu>
2023-11-14 23:33:18 -0500

    Add quat<->matrix3 functions to replace scipy

--- arena/utils/utils.py
+++ arena/utils/utils.py
@@ -1,6 +1,5 @@
 # simple general purpose functions
 import numpy as np
-from scipy.spatial.transform import Rotation
 
 
 class Utils(object):
@@ -32,9 +31,47 @@ class Utils(object):
         return d
 
     @classmethod
+    def quat_to_matrix3(cls, rotq):
+        x, y, z, w = rotq
+
+        # Compute matrix elements
+        xx, xy, xz = x * x, x * y, x * z
+        yy, yz, zz = y * y, y * z, z * z
+        wx, wy, wz = w * x, w * y, w * z
+
+        # Rotation matrix
+        rot_matrix = np.array(
+            [
+                [1 - 2 * (yy + zz), 2 * (xy - wz), 2 * (xz + wy)],
+                [2 * (xy + wz), 1 - 2 * (xx + zz), 2 * (yz - wx)],
+                [2 * (xz - wy), 2 * (yz + wx), 1 - 2 * (xx + yy)],
+            ]
+        )
+
+        return rot_matrix
+
+    @classmethod
+    def matrix3_to_quat(cls, rotm):
+        m00, m01, m02 = rotm[0, 0], rotm[0, 1], rotm[0, 2]
+        m10, m11, m12 = rotm[1, 0], rotm[1, 1], rotm[1, 2]
+        m20, m21, m22 = rotm[2, 0], rotm[2, 1], rotm[2, 2]
+
+        # Compute quaternion components
+        w = np.sqrt(max(0, 1 + m00 + m11 + m22)) / 2
+        x = np.sqrt(max(0, 1 + m00 - m11 - m22)) / 2
+        y = np.sqrt(max(0, 1 - m00 + m11 - m22)) / 2
+        z = np.sqrt(max(0, 1 - m00 - m11 + m22)) / 2
+
+        x = np.copysign(x, m21 - m12)
+        y = np.copysign(y, m02 - m20)
+        z = np.copysign(z, m10 - m01)
+
+        return np.array([x, y, z, w])
+
+    @classmethod
     def pose_to_matrix4(cls, pos, rotq, scale=(1, 1, 1)):  # Def arg not mutated
         mat = np.identity(4)
-        mat[0:3, 0:3] = Rotation.from_quat([rotq.x, rotq.y, rotq.z, rotq.w]).as_matrix()
+        mat[0:3, 0:3] = cls.quat_to_matrix3([rotq.x, rotq.y, rotq.z, rotq.w])
         mat[0:3, 3] = [pos.x, pos.y, pos.z]
         if scale != (1, 1, 1):
             scale_mat = np.identity(4)
@@ -45,7 +82,7 @@ class Utils(object):
     @classmethod
     def matrix4_to_pose(cls, mat):
         pos = mat[0:3, 3]
-        rotq = Rotation.from_matrix(mat[0:3, 0:3]).as_quat()
+        rotq = cls.matrix3_to_quat(mat[0:3, 0:3])
         scale = np.sqrt(np.sum(mat[0:3, 0:3] ** 2, axis=0))
         return pos, rotq, scale
 

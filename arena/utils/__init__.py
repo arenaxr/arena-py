aa891ada1d7c7b3ca0590ab5fbe95971994a7241
Nuno Pereira <nampereira@gmail.com>
2024-01-09 11:49:34 +0000

    fix: missing files for small refactor: env_vars to env

--- arena/utils/__init__.py
+++ arena/utils/__init__.py
@@ -1,4 +1,4 @@
 from .utils import Utils
 from .cmd_interpreter import ArenaCmdInterpreter
 from .arena_telemetry import ArenaTelemetry
-from .program_stats import ProgramStats
+from .program_info import ProgramRunInfo

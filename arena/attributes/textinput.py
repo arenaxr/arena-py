ed36a595aa38f8bf1c5e8fd90ead2ad3b69dfb68
mwfarb <mwfarb@andrew.cmu.edu>
2024-01-19 11:49:09 -0500

    add missing text input alias

--- arena/attributes/textinput.py
+++ arena/attributes/textinput.py
@@ -5,7 +5,7 @@ class Textinput(Attribute):
     """
     Textinput attribute class to manage its properties in the ARENA: Opens an HTML prompt when clicked. Sends text input as an event on MQTT. Requires click-listener.
     Usage: textinput=Textinput(...)
-    
+
     :param str on: A case-sensitive string representing the event type to listen for, e.g. 'mousedown', 'mouseup'. See https://developer.mozilla.org/en-US/docs/Web/Events [mousedown, mouseup, mouseenter, mouseleave, triggerdown, triggerup, gripdown, gripup, menudown, menuup, systemdown, systemup, trackpaddown, trackpadup]; defaults to 'mousedown' (optional)
     :param str title: The prompt title; defaults to 'Text Input' (optional)
     :param str label: Text prompt label; defaults to 'Input text below (max is 140 characters)' (optional)
@@ -20,3 +20,10 @@ class Textinput(Attribute):
                  **kwargs):
         super().__init__(title=title[:140], label=label[:140],
                          placeholder=placeholder[:140], on=on, **kwargs)
+
+
+class TextInput(Textinput):
+    """
+    Alternate name for Textinput.
+    Usage: textinput=TextInput(...)
+    """

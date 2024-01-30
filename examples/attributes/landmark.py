bec9af08fdda56122a51b7e515a73a1c6d460d58
mwfarb <mwfarb@andrew.cmu.edu>
2024-01-30 13:08:20 -0500

    system-tests: updated some tests

--- examples/attributes/landmark.py
+++ examples/attributes/landmark.py
@@ -10,7 +10,7 @@ object_id = "the_box"
 
 @scene.run_once
 def make_box():
-    scene.add_landmark(
-        Box(object_id=object_id, position=(0,2,-2)), label="The Box")
+    scene.add_object(
+        Box(object_id=object_id, position=(0,2,-2), landmark=Landmark(label="The Box")))
 
 scene.run_tasks()

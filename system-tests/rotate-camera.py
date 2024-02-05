7e2b4794607ca67311552b6577439c66fe387b54
mwfarb <mwfarb@andrew.cmu.edu>
2024-02-05 17:26:31 -0500

    Update rotate-camera.py

--- system-tests/rotate-camera.py
+++ system-tests/rotate-camera.py
@@ -7,9 +7,9 @@ import math
 from arena import *
 
 
-radius = 3
+radius = 10
 interval = 0
-
+sign = 1
 
 def user_join_callback(camera):
     print(f"User found: {camera.displayName} [object_id={camera.object_id}]")
@@ -18,29 +18,33 @@ def user_join_callback(camera):
 scene = Scene(host="arena-dev1.conix.io", scene="test", debug=True)
 scene.user_join_callback = user_join_callback
 
-
 @scene.run_forever(interval_ms=17)
 def move_cams():
-    global interval
+    global interval, sign
     for c in scene.users:
+        xp = radius*math.cos(interval)
+        zp = radius*math.sin(interval)
         scene.manipulate_camera(
             c,
             position=(
-                radius*math.cos(interval),
+                xp,
                 3,
-                radius*math.sin(interval),
+                zp,
             ),
-            # rotation=Rotation(0,0,0)
+            #rotation=Rotation(0,0,0)
         )
         scene.look_at(
             c,
-            target={"x": 0, "y": 0, "z": 0}
+            # target={"x": 0, "y": 0, "z": 0}
             # target=(0, 0, 0)
+            target=(xp, 0, zp)
         )
-    if interval > 6.28:
-        interval = 0
-    else:
-        interval = interval + 0.001
+    if interval > 3.14:
+        sign = -1
+    elif interval < 0:
+        sign = 1
+
+    interval = interval + (0.01*sign)
 
 
 scene.run_tasks()

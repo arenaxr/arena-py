049f2e4e9224bca889cb1b2bf541686fe8293403
mwfarb <mwfarb@andrew.cmu.edu>
2024-02-08 09:17:32 -0500

    chore: add multisrc example

---
+++ examples/attributes/multisrc.py
@@ -0,0 +1,20 @@
+from arena import *
+
+scene = Scene(host="arenaxr.org", scene="example")
+
+
+@scene.run_once
+def make_die1():
+    die1 = Box(
+        object_id="die1",
+        position=Position(0, 3,  -2),
+        material=Material(color="#ffffff"),
+        multisrc=Multisrc(
+            srcspath="store/users/wiselab/images/dice/",
+            srcs="side1.png, side2.png, side3.png, side4.png, side5.png, side6.png"
+        )
+    )
+    scene.add_object(die1)
+
+
+scene.run_tasks()

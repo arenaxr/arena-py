ddda9f85f88d3facc7a1127cf1f67c8e05d764e4
mwfarb <mwfarb@andrew.cmu.edu>
2024-02-09 15:53:36 -0500

    chore: fix working example model update

--- examples/attributes/modelUpdate.py
+++ examples/attributes/modelUpdate.py
@@ -9,26 +9,16 @@ def make_model_update():
         object_id="model_update_draco",
         position=(0, 2, -3),
         url="/store/models/robot-draco.glb",
+        # joint names might need some characters removed like ':' from 'mixamorig:LeftShoulder'
         modelUpdate={
-            "mixamorig:LeftShoulder": {
+            "mixamorigLeftShoulder": {
                 "position": {"x": 1, "y": 0, "z": 0},
-                "rotation": {"x": 0, "y": 1, "z": 0, "w": 0}
+                "rotation": {"x": 1, "y": 0, "z": 0, "w": 0}
             },
-            "mixamorig:RightShoulder": {
-                "rotation": {"x": -1, "y": 0, "z": 0, "w": 0}
+            "mixamorigRightLeg": {
+                "rotation": {"x": -0.7, "y": 0, "z": 0, "w": 0.7}
             },
         },
-        # TODO (mwfarb): neither of these examples seems to work
-        # url="store/models/BrainStem.glb",
-        # modelUpdate={
-        #     "joint4": {
-        #         "position": {"x": 1, "y": 0, "z": 0},
-        #         "rotation": {"x": 0, "y": 1, "z": 0, "w": 0}
-        #     },
-        #     "joint6": {
-        #         "rotation": {"x": -1, "y": 0, "z": 0, "w": 0}
-        #     },
-        # },
     )
     scene.add_object(model_update)
 

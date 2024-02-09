6940adeb37db54dd35a3e05235c27679798e958c
mwfarb <mwfarb@andrew.cmu.edu>
2024-02-09 11:01:14 -0500

    chore: add interactive launcher scripts

---
+++ launchers/README.md
@@ -0,0 +1,3 @@
+ARENA-py Script Launchers
+=================
+These are sample launcher scripts we have used from time to time to run programs when demonstrating the interactive programmability features of ARENA.
---
+++ launchers/public-arena-guac-plus.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Launch multiple programs per scene
+
+host="arenaxr.org"
+namespace="public"
+scene="arena"
+
+export MQTTH=$host
+export NAMESPACE=$namespace
+export SCENE=$scene
+
+# prime auth
+python3 -c "from arena import *; Scene(host='$host',namespace='$namespace',scene='$scene')"
+
+# define apps
+statue="python3 examples/statue.py -mh $host -n $namespace -s $scene -p 0 0 0 -r 0 0 0 -s 0 0 0"
+laser="python3 examples/laser-pointer.py"
+green="python3 examples/green-boxes.py"
+pinata="python3 examples/pinata.py"
+earth="python3 examples/earth-moon.py -mh $host -n $namespace -s $scene -p 0 0.5 0  -r 0 0 0   -c 0.25 0.25 0.25"
+robot="python3 examples/arm-demo.py -mh $host -n $namespace -s $scene   -p -0.5 0.2 -2.75  -r 0 0 0   -c 0.25 0.25 0.25"
+guac="python3 examples/guac.py -mh $host -n $namespace -s $scene        -p 0.75 0.5 0 -r 0 180 0   -c 0.5 0.5 0.5"
+
+# launch all apps
+# Press Ctrl+C to kill them all
+sh -c "$robot & $guac & wait"

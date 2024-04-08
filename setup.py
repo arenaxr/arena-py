1aa1c8874025769067bdc672268acc9f532c7070
mwfarb <mwfarb@andrew.cmu.edu>
2024-04-08 11:49:45 -0400

    fix: bump version from removing pypi tests

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="arena-py",
-    version="0.9.5",
+    version="0.9.6",
     author="Carnegie Mellon University",
     author_email="arenaxr@andrew.cmu.edu",
     license="BSD 3-clause \"New\" or \"Revised License\"",

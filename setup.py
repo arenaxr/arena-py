54f8aafbdb061e95d97efac1b3b0d9266aef8dfa
mwfarb <mwfarb@andrew.cmu.edu>
2024-04-08 11:35:29 -0400

    fix: bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("README.md", "r") as readme:
 
 setuptools.setup(
     name="arena-py",
-    version="0.9.4",
+    version="0.9.5",
     author="Carnegie Mellon University",
     author_email="arenaxr@andrew.cmu.edu",
     license="BSD 3-clause \"New\" or \"Revised License\"",

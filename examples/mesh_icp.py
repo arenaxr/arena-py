73d6ccdd06b51c316c2a7f3db5c25a0dfd8837ed
Ivan Liang <r33t@cmu.edu>
2024-05-07 19:34:25 -0400

    Update mesh ICP:
    - Add filtering by y-axis rotation, not to exceed some deg threshold
    - Print inlier_rmse (a lot more important when many fitness hit 1.0)
    - Fix CUDA switches
    - Change poisson sampling to target a density, not total points
    - Crop mesh on all axes bounds for quest
    - Set target distance based on bounding box size
    - Use TukeyLoss robust kernel for outlier handling
    - Increase max iterations to 100

--- examples/mesh_icp.py
+++ examples/mesh_icp.py
@@ -22,6 +22,30 @@ target_distance = None
 cuda_device = o3d.core.Device("CUDA:0")
 
 
+# This removes artificial boundary box created in Quest3 room meshes
+QUEST_CROP = 0.06
+
+
+def filter_registration_results(results, threshold=4):
+    filtered_results = []
+    for result in results:
+        if USE_CUDA:
+            transformation_matrix = result.transformation.cpu().numpy()
+        else:
+            transformation_matrix = result.transformation
+        rotation_matrix = np.array(transformation_matrix[:3, :3])
+        rot = R.from_matrix(rotation_matrix)
+        euler_angles = rot.as_euler("xyz", degrees=True)
+        x_rotation, _, z_rotation = abs(euler_angles)
+        if (180 - threshold) > abs(x_rotation) > threshold:
+            continue
+        if (180 - threshold) > abs(z_rotation) > threshold:
+            continue
+        filtered_results.append(result)
+    print(f"Filtered out {len(results) - len(filtered_results)} results")
+    return filtered_results
+
+
 def msg_callback(_client, _userdata, msg):
     """
     Callback for incoming mesh data from pubsub. Will attempt to align incoming source
@@ -67,9 +91,11 @@ def msg_callback(_client, _userdata, msg):
             time.time() - start_time,
             ". ICP result for",
             usercam,
-            ": fitness = ",
+            ": fitness:",
             res.fitness,
-            ", transform = ",
+            ", inlier_rmse:",
+            res.inlier_rmse,
+            ", transform:\n",
             res.transformation,
         )
 
@@ -119,7 +145,11 @@ def draw_registration_result(source, icp_transform, uniform_color=None):
         return
     if uniform_color is None:
         uniform_color = [0, 0, 1]
-    source_transformed = o3d.geometry.PointCloud(source)
+    if USE_CUDA:
+        source_transformed = source.to_legacy()
+        icp_transform = icp_transform.cpu().numpy()
+    else:
+        source_transformed = o3d.geometry.PointCloud(source)
     source_transformed.transform(icp_transform)
     source_transformed.paint_uniform_color(uniform_color)
     vis.add_geometry(source_transformed)
@@ -176,29 +206,33 @@ def load_mesh_data(mesh_data, write=False, target=False):
     return mesh
 
 
-def create_pcd(mesh, points=10000, write=False, crop_y=0.5):
+def create_pcd(mesh, point_density=300, write=False, crop_bounds=QUEST_CROP):
     """
     Creates a point cloud from a mesh using poisson disk sampling
     :param mesh: mesh to convert
-    :param int points: how many points to produce for resuling point cloud
+    :param int point_density: How many points per square meter of surface area
     :param bool write: whether to write the point cloud to a pcd file
-    :param float crop_y: how much to crop off the top and bottom of the mesh
+    :param float crop_bounds: how much to crop off the bounding box on all sides
     :return: Open3D PointCloud
     """
+    area = mesh.get_surface_area()
+    points = int(area * point_density)
     pcd = mesh.sample_points_poisson_disk(
         number_of_points=points, use_triangle_normal=True
     )
 
-    if crop_y > 0:  # Cropping floor and ceiling helps remove useless points for ICP
+    if crop_bounds > 0:
+        obb = pcd.get_minimal_oriented_bounding_box(robust=True)
+        pcd.rotate(np.linalg.inv(obb.R))
         aabb = pcd.get_axis_aligned_bounding_box()
         min_bound = np.array(aabb.min_bound)
         max_bound = np.array(aabb.max_bound)
-        min_bound[1] += crop_y  # Increase the lower Y-bound
-        max_bound[1] -= crop_y  # Decrease the upper Y-bound
-
-        # Crop 0.5m off ceiling and floor
-        cropped_aabb = o3d.geometry.AxisAlignedBoundingBox(min_bound, max_bound)
-        pcd = pcd.crop(cropped_aabb)
+        min_bound += crop_bounds
+        max_bound -= crop_bounds
+        aabb.min_bound = min_bound
+        aabb.max_bound = max_bound
+        pcd = pcd.crop(aabb)
+        pcd.rotate(obb.R)
 
     if write:
         print("Writing target.pcd")
@@ -232,15 +266,14 @@ def set_target_distance():
     """
     global target_distance
     if target_pcd is not None:
-        obb = target_pcd.get_oriented_bounding_box()
-        obb_dimensions = obb.extent
-        w, h, d = obb_dimensions
-        # Factor in surface area of the box with point cloud density
-        target_distance = (w * h + w * d + h * d) / (2 * 10000**0.5)
+        target_dims = target_pcd.get_axis_aligned_bounding_box().get_extent()
+        target_distance = np.linalg.norm(target_dims) / 2
+        if USE_CUDA:
+            target_distance = target_distance.item()
         print("Target distance: ", target_distance)
 
 
-def icp(src, target, distance=0, rotations=8):
+def icp(src, target, distance=0, rotations=6):
     """
     Given a source and target point cloud, attempts to align the source to the target
     :param src: source point cloud
@@ -275,13 +308,19 @@ def icp(src, target, distance=0, rotations=8):
     attempts = []
     rot_matrix = rotation_matrix_y(360 / rotations)
 
+    # Tukey loss function for robustness
+    sigma = 1
+    if not USE_CUDA:
+        loss = o3d.pipelines.registration.TukeyLoss(k=sigma)
+        p2pl = o3d.pipelines.registration.TransformationEstimationPointToPlane(loss)
+
     # Try each rotation as initial transform and take the best result
     for i in range(rotations):
         # Incrementally rotate while preserving translation
         init_transform = rot_matrix @ init_transform
         if USE_CUDA:
             init_transform_cuda = o3d.core.Tensor(
-                init_transform, device=cuda_device, dtype=o3d.core.Dtype.Float32
+                init_transform, dtype=o3d.core.Dtype.Float64
             )
             res = o3d.t.pipelines.registration.icp(
                 src,
@@ -289,7 +328,9 @@ def icp(src, target, distance=0, rotations=8):
                 max_correspondence_distance=max_distance,
                 init_source_to_target=init_transform_cuda,
                 estimation_method=o3d.t.pipelines.registration.TransformationEstimationPointToPlane(),
-                # criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200),
+                criteria=o3d.t.pipelines.registration.ICPConvergenceCriteria(
+                    max_iteration=100
+                ),
             )
         else:
             res = o3d.pipelines.registration.registration_icp(
@@ -297,13 +338,25 @@ def icp(src, target, distance=0, rotations=8):
                 target,
                 max_correspondence_distance=max_distance,
                 init=init_transform,
-                estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPlane(),
-                # criteria=o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=200),
+                estimation_method=p2pl,
+                criteria=o3d.pipelines.registration.ICPConvergenceCriteria(
+                    max_iteration=100
+                ),
             )
         attempts.append(res)
         # draw_registration_result(src, res.transformation, uniform_color=[0, 0, res.fitness])
 
-    return max(attempts, key=lambda x: x.fitness)
+    attempts = filter_registration_results(attempts)
+    if len(attempts) == 0:
+        res = o3d.pipelines.registration.RegistrationResult()
+        res.fitness = 0
+        res.inlier_rmse = 0
+        res.transformation = np.identity(4)
+        return res
+    # Handle ties for maxed fitness by lowest inlier_rmse
+    max_fitness = max(attempts, key=lambda x: x.fitness).fitness
+    matches = [x for x in attempts if x.fitness == max_fitness]
+    return min(matches, key=lambda x: x.inlier_rmse)
 
 
 scene = Scene(namespace="public", scene="arena")
@@ -357,23 +410,34 @@ if target_pcd is not None:
 #     print("ICP fitness: ", res.fitness, ", transform:", res.transformation)
 #     draw_registration_result(src_pcd, res.transformation)
 
-# src_mesh = o3d.io.read_triangle_mesh("./meshes/meshes_1701384568.glb")
+# src_mesh = o3d.io.read_triangle_mesh("./meshes/mesh_64941_131610.glb")
 # if not src_mesh.has_triangle_normals():
 #     src_mesh.compute_triangle_normals()
 # if not src_mesh.has_vertex_normals():
 #     src_mesh.compute_vertex_normals()
 # src_pcd = create_pcd(src_mesh)
+# o3d.io.write_point_cloud("src.pcd", src_pcd)
+#
+# src_pcd = o3d.io.read_point_cloud("src.pcd")
+# RANDOM_ROTATE = 50
+# if RANDOM_ROTATE:
+#     src_pcd.rotate(rotation_matrix_y(RANDOM_ROTATE)[:3, :3])
 # src_pcd.paint_uniform_color([1, 0, 0])
-# src_pcd_cuda = o3d.t.geometry.PointCloud().from_legacy(src_pcd)
+# vis.add_geometry(src_pcd)
+# if USE_CUDA:
+#     src_pcd = o3d.t.geometry.PointCloud().from_legacy(src_pcd)
+#     target_pcd = target_pcd_cuda
 # start_time = time.time()
-# res = icp(src_pcd_cuda, target_pcd_cuda)
+# res = icp(src_pcd, target_pcd)
 # print(
-#     "Execution time: ",
+#     "Execution time:",
 #     time.time() - start_time,
-#     ". ICP fitness: ",
+#     ", fitness:",
 #     res.fitness,
-#     ", transform:",
-#     res.transformation
+#     ", inlier_rmse:",
+#     res.inlier_rmse,
+#     ", transform:\n",
+#     res.transformation,
 # )
 # draw_registration_result(src_pcd, res.transformation)
 

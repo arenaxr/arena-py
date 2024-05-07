b4f9b1fbe5e29093a2f07b75dc8ec138bf3e31e4
mwfarb <mwfarb@andrew.cmu.edu>
2024-05-07 12:07:52 -0400

    chore: fix cobot script

--- launchers/public-arena-npc-chess.sh
+++ launchers/public-arena-npc-chess.sh
@@ -14,16 +14,16 @@ python3 -c "from arena import *; Scene(host='$host',namespace='$namespace',scene
 
 # run from anywhere
 chess="python3 examples/demos/chess/chess.py -mh $host -n $namespace -s $scene -p 1.5 0 -1.5 -r 0 45 0 -c .15 .15 .15"
-cobot="python3 examples/demos/cobot-pi/MyCobotPi320.py"
 npc_arena="python3 examples/demos/npc/NPC.py examples/demos/npc/ArenaRobot"
 
 # run from rpi
-cobot_rpi="bash examples/demos/cobot-pi/autostart_python.sh"
+cobot_rpi_start="bash examples/demos/cobot-pi/autostart_python.sh"
+cobot_rpi_run="python3 examples/demos/cobot-pi/MyCobotPi320.py"
 
 # run from arduno connected to COM4
 car_arduino="python3 examples/demos/bosch-car/Python/BoschCar.py"
 
 # launch all apps
 # Press Ctrl+C to kill them all
-sh -c "$chess & $cobot& $npc_arena & wait"
+sh -c "$chess & $npc_arena & wait"
 # parameters to run demos in the arena main demo scene

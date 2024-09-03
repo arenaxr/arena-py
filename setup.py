f9c89f4409e67584fae6bf36a7b685bf787307c3
Ivan Liang <r33t@cmu.edu>
2024-09-03 16:52:55 -0400

    Bump python_requires to 3.10 for union type hints

--- setup.py
+++ setup.py
@@ -49,5 +49,5 @@ setuptools.setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.8',
+    python_requires='>=3.10',
 )

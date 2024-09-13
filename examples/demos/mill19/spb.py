e4cd6c2a1555432dbafb3f35e561bbfecd1fd3a8
mwfarb <mwfarb@andrew.cmu.edu>
2024-09-13 15:25:17 -0400

    updated tests mill 19

--- examples/demos/mill19/spb.py
+++ examples/demos/mill19/spb.py
@@ -1,7 +1,9 @@
-from arena import *
-from mqtt_spb_wrapper import *
 import time
 
+import numpy as np
+from mqtt_spb_wrapper import *
+
+from arena import *
 
 domain_name = "CMU Devices"
 app_entity_name = "ROS1 Node"
@@ -54,12 +56,14 @@ mmjoints = {
     # 'joint_6_t-flange',  # limit: {lower: 0, upper: 0}, fixed
 }
 
+mo = (0, -.66, 0)
+#mo = (0, -10, 0)
 
-# {"object_id":"motoman","persist":true,"type":"object","action":"update","data":{"object_type":"urdf-model","url":"store/users/mwfarb/xacro/motoman_gp4_support/urdf/gp4.xacro","urlBase":"/store/users/mwfarb/xacro/motoman_gp4_support","position":{"x":6.22109,"y":5.30667,"z":16.84618},"rotation":{"w":0.707,"x":-0.707,"y":0,"z":0},"scale":{"x":1,"y":1,"z":1}}}
+# ########### moto_arch ##########
 moto_arch = UrdfModel(
     object_id="moto_arch",
     # position={"x": 7, "y": 6.40, "z": 16.25},
-    position={"x": 0, "y": 0.66, "z": -0.45},
+    position={"x": 0+mo[0], "y": 0.66+mo[1], "z": -0.45+mo[2]},
     # rotation={"x": -90, "y": 180, "z": 0},
     rotation={"x": -90, "y": -90, "z": 0},
     scale={"x": 0.8, "y": 0.8, "z": 0.8},
@@ -72,19 +76,35 @@ moto_arch_sign = ArenauiCard(
     parent=moto_arch.object_id,
     title="Motoman Architect",
     body="Awaiting status update...",
-    position=(-0.25, 0, 1),
+    position=(-0.25, 0, 1.5),
     look_at="#my-camera",
     persist=True,
     widthScale=0.5,
 )
 
+# ########### moto_dest ##########
+moto_dest_base = Object(
+    object_id="moto_dest_base",
+    # position={"x": 6.22, "y": 6.40, "z": 16.25},
+    position={"x": 0+mo[0], "y": 0.66+mo[1], "z": 0.4+mo[2]},
+    scale={"x": 0.8, "y": 0.8, "z": 0.8},
+    animation=None,
+    # animation={
+    #     "property": "rotation",
+    #     "from": "0 0 0",
+    #     "to": "0 360 0",
+    #     "loop": True,
+    #     "dur": 20000,
+    #     "easing": "linear"
+    # },
+    persist=True,
+)
 moto_dest = UrdfModel(
     object_id="moto_dest",
-    # position={"x": 6.22, "y": 6.40, "z": 16.25},
-    position={"x": 0, "y": 0.66, "z": 0.4},
+    parent="moto_dest_base",
     # rotation={"x": -90, "y": 0, "z": 0},
     rotation={"x": -90, "y": 90, "z": 0},
-    scale={"x": 0.8, "y": 0.8, "z": 0.8},
+    scale={"x": 1, "y": 1, "z": 1},
     url="store/users/mwfarb/xacro/motoman_gp4_support/urdf/gp4.xacro",
     urlBase="/store/users/mwfarb/xacro/motoman_gp4_support",
     persist=True,
@@ -92,22 +112,172 @@ moto_dest = UrdfModel(
 moto_dest_sign = ArenauiCard(
     object_id="moto_dest_sign",
     parent=moto_dest.object_id,
-    title="Motoman Destroyer",
+    title="Motoman GP7 GP8",
     body="Awaiting status update...",
-    position=(-0.25, 0, 1),
+    position=(0, 0, 1.5),
     look_at="#my-camera",
     persist=True,
-    widthScale=0.5,
+)
+
+# ########### moto_dest sensor ##########
+moto_dest_sensor = Sphere(
+    object_id="moto_dest_sensor",
+    parent=moto_dest.object_id,
+    position=(.2, .2, 0),
+    scale={"x": .1, "y": .1, "z": .1},
+    material={"color": "#00ff00", "opacity": .75},
+    clickable=True,
+    persist=True,
+    remote_render={"enabled": False},
 )
 
 
-@scene.run_once
+def sensor_on_callback(scene, evt, msg):
+    sensor_animate(scene, evt, "#00ff00", "#ff0000", 2000)
+
+
+def sensor_off_callback(scene, evt, msg):
+    sensor_animate(scene, evt, "#ff0000", "#00ff00", 500)
+
+
+def sensor_animate(scene, evt, start, end, dur):
+    global moto_dest_sensor
+    if evt.type == "mousedown":
+        moto_dest_sensor.update_attributes(
+            animation={
+                "property": "components.material.material.color",
+                "type": "color",
+                "from": start,
+                "to": end,
+                "dur": dur,
+                "easings": "easeInOutBounce",
+                "loop": "once",
+                "autoplay": True,
+            },
+        )
+        scene.update_object(moto_dest_sensor)
+
+
+moto_dest_on = Box(
+    object_id="moto_dest_on",
+    position={"x": (0 - 4), "y": 0.66, "z": 0.4},
+    # position={"x": (6.22 - 4), "y": 6.40, "z": 16.25},
+    scale={"x": .1, "y": .1, "z": .1},
+    material={"color": "#00ff00"},
+    evt_handler=sensor_on_callback,
+    clickable=True,
+    persist=True,
+    remote_render={"enabled": False},
+)
+moto_dest_off = Box(
+    object_id="moto_dest_off",
+    position={"x": (0 - 4.1), "y": 0.66, "z": 0.4},
+    # position={"x": (6.22 - 4.1), "y": 6.40, "z": 16.25},
+    scale={"x": .1, "y": .1, "z": .1},
+    material={"color": "#ff0000"},
+    evt_handler=sensor_off_callback,
+    clickable=True,
+    persist=True,
+    remote_render={"enabled": False},
+)
+
+# ########### AMR MP400 ##########
+# {"x": 5.20, "y": 4.60, "z": 17.90},
+# {"x": 5.20, "y": 4.60, "z": 12.90},
+# {"x": 7.95, "y": 4.60, "z": 12.90},
+# {"x": 7.95, "y": 4.60, "z": 17.90},
+x1 = 1.1
+x2 = -3
+y1 = -1
+z1 = -2
+z2 = 1.5
+wps = [
+    {"x": x1, "y": y1, "z": z1},
+    {"x": x1, "y": y1, "z": z2},
+    {"x": x2, "y": y1, "z": z1},
+    {"x": x2, "y": y1, "z": z2},
+]
+mp400 = UrdfModel(
+    object_id="mp400",
+    # position=wps[0],
+    position=(1.1, -1, -2),
+    rotation={"x": -90, "y": 90, "z": 0},
+    scale={"x": 1, "y": 1, "z": 1},
+    url="store/users/mwfarb/xacro/neo_mp_400/robot_model/mp_400/mp_400.urdf.xacro",
+    urlBase="/store/users/mwfarb/xacro/neo_mp_400",
+    persist=True,
+)
+mp400_sign = ArenauiCard(
+    object_id="mp400_sign",
+    # parent=mp400.object_id,
+    title="Mobile Robot MP-400",
+    body="Awaiting status update...",
+    position=(wps[0]['x'], wps[0]['y']+2, wps[0]['z']),
+    look_at="#my-camera",
+    persist=True,
+)
+
+
+@ scene.run_forever(interval_ms=100)
+def update_mp400():
+    t = time.time()
+    # move mp400 along floor
+    offset = math.pi
+    ratio = math.sin(t + offset)
+    if -1 <= ratio <= -.5:
+        wp1 = 0
+        wp2 = 1
+        r = -90
+    elif -.5 <= ratio <= 0:
+        wp1 = 1
+        wp2 = 2
+        r = -180
+    elif 0 <= ratio <= .5:
+        wp1 = 2
+        wp2 = 3
+        r = -270
+    else:
+        wp1 = 3
+        wp2 = 0
+        r = 0
+    xp = [-1+(wp1*.5), -.5+(wp1*.5)]
+
+    x = np.interp(ratio, xp, [wps[wp1]['x'], wps[wp2]['x']])
+    z = np.interp(ratio, xp, [wps[wp1]['z'], wps[wp2]['z']])
+    position = {'x': x, 'y': wps[wp1]['y'], 'z': z}
+    rotation = {'x': -90, 'y': r, 'z': 0}
+    mp400.update_attributes(
+        position=position, rotation=rotation, persist=False)
+    scene.update_object(mp400)
+    mp400_sign.update_attributes(
+        body=json.dumps({'position': position, 'rotation': rotation}, indent=4), persist=False)
+    scene.update_object(mp400_sign)
+
+
+# ########### INIT ##########
+@ scene.run_once
 def main():
     scene.add_object(moto_arch)
     scene.add_object(moto_arch_sign)
+    scene.add_object(moto_dest_base)
     scene.add_object(moto_dest)
+    scene.add_object(moto_dest_sensor)
     scene.add_object(moto_dest_sign)
+    scene.add_object(moto_dest_on)
+    scene.add_object(moto_dest_off)
+    scene.add_object(mp400)
+    scene.add_object(mp400_sign)
+    scene.add_object(
+        Sphere(scale=(.1, .1, .1), position=wps[0], remote_render={"enabled": False}))
+    scene.add_object(
+        Sphere(scale=(.1, .1, .1), position=wps[1], remote_render={"enabled": False}))
+    scene.add_object(
+        Sphere(scale=(.1, .1, .1), position=wps[2], remote_render={"enabled": False}))
+    scene.add_object(
+        Sphere(scale=(.1, .1, .1), position=wps[3], remote_render={"enabled": False}))
 
+
+# ########### MQTT motoman Bridge ##########
 
 last_arch = ""
 last_dest = ""
@@ -148,14 +318,16 @@ def callback_app_message(topic, payload):
                     last_arch = str_mmj_arch
                     moto_arch.update_attributes(joints=str_mmj_arch)
                     updates.append(moto_arch)
-                    moto_arch_sign.update_attributes(body="\n".join(mmj_arch).replace(":", "\t"))
+                    moto_arch_sign.update_attributes(
+                        body="\n".join(mmj_arch).replace(":", "\t"))
                     updates.append(moto_arch_sign)
 
                 if len(mmj_dest) > 0 and str_mmj_dest != last_dest:
                     last_dest = str_mmj_dest
                     moto_dest.update_attributes(joints=str_mmj_dest)
                     updates.append(moto_dest)
-                    moto_dest_sign.update_attributes(body="\n".join(mmj_dest).replace(":", "\t"))
+                    moto_dest_sign.update_attributes(
+                        body="\n".join(mmj_dest).replace(":", "\t"))
                     updates.append(moto_dest_sign)
 
                 scene.update_objects(updates)

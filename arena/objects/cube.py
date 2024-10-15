c85bf1b3d18862e7917f9aa93c07d0369fea332c
mwfarb <mwfarb@andrew.cmu.edu>
2024-10-15 14:30:35 -0400

    chore: update each cube attr as deprecated

--- arena/objects/cube.py
+++ arena/objects/cube.py
@@ -8,12 +8,12 @@ class Cube(Object):
     """
     Cube object class to manage its properties in the ARENA: Cube (=Box) Geometry (**deprecated**); Supported for Legacy reasons; Please use Box in new scenes
 
-    :param float depth: depth Defaults to '1' (optional)
-    :param float height: height Defaults to '1' (optional)
-    :param int segmentsDepth: segments depth Defaults to '1' (optional)
-    :param int segmentsHeight: segments height Defaults to '1' (optional)
-    :param int segmentsWidth: segments width Defaults to '1' (optional)
-    :param float width: width Defaults to '1' (optional)
+    :param float depth: *depth* Defaults to '1' (deprecated)
+    :param float height: *height* Defaults to '1' (deprecated)
+    :param int segmentsDepth: *segments depth* Defaults to '1' (deprecated)
+    :param int segmentsHeight: *segments height* Defaults to '1' (deprecated)
+    :param int segmentsWidth: *segments width* Defaults to '1' (deprecated)
+    :param float width: *width* Defaults to '1' (deprecated)
     """
     object_type = "cube"
 

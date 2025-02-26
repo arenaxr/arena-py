4a8a24737b6fbbe4d7962b6e4387be6e07f0727e
mwfarb <mwfarb@andrew.cmu.edu>
2025-02-26 15:03:00 -0500

    chore: fix object_id example

--- examples/basic/object_id.py
+++ examples/basic/object_id.py
@@ -8,15 +8,15 @@ scene = Scene(host="arenaxr.org", scene="example")
 
 
 @scene.run_once
-def make_timed_sphere():
+def make_named_boxes():
     # create a box with a uuid generated name
-    box_named_id = Box()
+    box_named_id = Box(
+        object_id="box1",
+    )
     scene.add_object(box_named_id)
 
     # create a box with the name "box1"
-    box_named_uuid = Box(
-        object_id="box1",
-    )
+    box_named_uuid = Box()
     scene.add_object(box_named_uuid)
 
 
